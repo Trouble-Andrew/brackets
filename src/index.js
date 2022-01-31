200ed2548a2030d1ce2c47d03f2f1c65d651e7e9
Trouble-Andrew <satyr663@gmail.com>
2022-01-31 10:35:45 +0300

    feat: add match brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let flag = true;
+
+  function removeBrackets() {
+    flag = false;
+    for (const brackets of bracketsConfig) {
+        let bracketsPair = `${brackets[0]}${brackets[1]}`;
+        while (str.indexOf(bracketsPair) >= 0) {
+            str = str.replace(bracketsPair, '');
+            flag = true;
+        }
+    }
+
+    if (str.length > 0 && flag === true) {
+        removeBrackets();
+    }        
+  }
+
+  removeBrackets();
+  return flag;
 }
